8a13f678fbb6b62986ec29b147f67f036ab213d9
Andre Pato <anpato1994@gmail.com>
2021-02-19 11:14:53 -0500

    update starter code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import './styles/App.css'
 import React, { Component } from 'react'
-import Games from './components/Games'
-import Nav from './components/Nav'
-import { Switch, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Genres from './components/Genres'
-import GameDetails from './components/GameDetails'
 
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        <Nav />
-        <main>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/games" component={Games} />
-            <Route path="/genres" component={Genres} />
-            <Route path="/games/:game_id" component={GameDetails} />
-          </Switch>
-        </main>
-      </div>
-    )
+    return <div></div>
   }
 }
