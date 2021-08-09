4c33dceb301bc833ba3767a2636d4278dd28a74c
Michael Lackey <michael.g.lackey@gmail.com>
2021-08-09 14:50:02 -0500

    convert to Hooks and clarify instructions

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
-export default class Nav extends Component {
-  render() {
-    return (
-      <header>
-        <div>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/games">Games</NavLink>
-            </li>
-            <li>
-              <NavLink to="/genres">Genres</NavLink>
-            </li>
-          </ul>
-        </div>
-      </header>
-    )
-  }
+
+const Nav = () => {
+  
+  return (
+    <header>
+      <div>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/games">Games</NavLink>
+          </li>
+          <li>
+            <NavLink to="/genres">Genres</NavLink>
+          </li>
+        </ul>
+      </div>
+    </header>
+  )
 }
+
+export default Nav;
